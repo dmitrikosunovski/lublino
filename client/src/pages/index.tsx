5e03daf1496ce9b0868b4a2877111ad02adc4a61
ivan <ivan.safonov.09@mail.ru>
2023-04-08 16:08:31 +0300

    add layout

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,22 +1,22 @@
-import { FirstScreen } from "@/Components/FirstScreen/FirstScreen";
-import { Header } from "@/Components/Header/Header";
-import { MapScreen } from "@/Components/MapScreen/MapScreen";
-import Form from "@/Components/Form/Form";
+import { Header } from '@/components/Header/Header'
 import styles from './Index.module.scss'
+import { FirstScreen } from '@/components/FirstScreen/FirstScreen'
+import Form from '@/components/Form/Form'
+import { MapScreen } from '@/components/MapScreen/MapScreen'
 
 export default function Home() {
   return (
     <div>
-    <div>
-    <Header />
-    <FirstScreen />
-    <div className={styles.formcont}>
       <div>
-      <Form/>
+        <Header />
+        <FirstScreen />
+        <div className={styles.formcont}>
+          <div>
+            <Form />
+          </div>
+        </div>
+        <MapScreen />
       </div>
-    </div>
-    <MapScreen />
-    </div>
     </div>
   )
 }
