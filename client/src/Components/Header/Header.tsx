5ebc04b3942772aa23cdada5145c54902ea31548
ivan <ivan.safonov.09@mail.ru>
2023-04-08 16:09:26 +0300

    fix css global-header

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -1,35 +1,40 @@
-import { MailIcon } from '../Icons/MailIcon';
-import styles from './Header.module.scss';
+import { MailIcon } from '../Icons/MailIcon'
+import styles from './Header.module.scss'
 import Image from 'next/image'
 
 export const Header = () => {
-    return (
-       <div className={styles.container}>
-        <div className={styles.topHeader}>
-            <div className={styles.logocont}>
-            <Image
-                src="/logo.png"
-                alt="Logo"
-                className={styles.logo}
-                width={350}
-                height={110}
-                priority
-              />
-            </div>
-            <div className={styles.rightcont}>
-                <div className={styles.contacts}>
-                <div className={styles.conttext}><h2>+7 (901) 719 96 11</h2></div>
-                <div className={styles.line}></div>
-                <div className={styles.conttext1}><MailIcon/><h3>contact@lublinoexpress.ru</h3></div>
-                </div>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.topHeader}>
+        <div className={styles.logocont}>
+          <Image
+            src='/logo.png'
+            alt='Logo'
+            className={styles.logo}
+            width={350}
+            height={110}
+            priority
+          />
         </div>
-        <div className={styles.lowHeader}>
-            <div className={styles.lowHeaderText}>Главная</div>
-            <div className={styles.lowHeaderText}>Калькулятор</div>
-            <div className={styles.lowHeaderText}>Отследить посылку</div>
-            <div className={styles.lowHeaderText}>Документы</div>
+        <div className={styles.rightcont}>
+          <div className={styles.contacts}>
+            <div className={styles.conttext}>
+              <h2>+7 (901) 719 96 11</h2>
+            </div>
+            <div className={styles.line}></div>
+            <div className={styles.conttext1}>
+              <MailIcon />
+              <h3>contact@lublinoexpress.ru</h3>
+            </div>
+          </div>
         </div>
-       </div> 
-    )
+      </div>
+      <div className={styles.lowHeader}>
+        <div className={styles.lowHeaderText}>Главная</div>
+        <div className={styles.lowHeaderText}>Калькулятор</div>
+        <div className={styles.lowHeaderText}>Отследить посылку</div>
+        <div className={styles.lowHeaderText}>Документы</div>
+      </div>
+    </div>
+  )
 }
