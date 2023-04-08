b114829310c4f613fd9cbb97900d84e5e9d6d6c3
ivan <ivan.safonov.09@mail.ru>
2023-04-09 00:25:10 +0300

    header adaptive

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -1,34 +1,27 @@
 import Link from 'next/link'
-import { MailIcon } from '../Icons/MailIcon'
 import styles from './Header.module.scss'
 import Image from 'next/image'
+import info from '../../assets/png/info.png'
 
 export const Header = () => {
   return (
     <div className={styles.container}>
       <div className={styles.topHeader}>
-        <div className={styles.logocont}>
-          <Image
-            src='/logo.png'
-            alt='Logo'
-            className={styles.logo}
-            width={350}
-            height={110}
-            priority
-          />
-        </div>
-        <div className={styles.rightcont}>
-          <div className={styles.contacts}>
-            <div className={styles.conttext}>
-              <h2>+7 (901) 719 96 11</h2>
-            </div>
-            <div className={styles.line}></div>
-            <div className={styles.conttext1}>
-              <MailIcon />
-              <h3>contact@lublinoexpress.ru</h3>
-            </div>
-          </div>
-        </div>
+        <Image
+          src='/logo.png'
+          alt='Logo'
+          width={327}
+          height={98}
+          priority
+          className={styles.logo}
+        />
+        <Image
+          src={info.src}
+          width={287}
+          height={82}
+          alt='info'
+          className={styles.info}
+        />
       </div>
       <div className={styles.lowHeader}>
         <Link href={'#'} className={styles.lowHeaderText}>
