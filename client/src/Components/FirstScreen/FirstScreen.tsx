d9690df9dda0dd8e160e2d1432e0a60233a2c300
ivan <ivan.safonov.09@mail.ru>
2023-04-09 13:55:38 +0300

    first screen adaptive

--- client/src/Components/FirstScreen/FirstScreen.tsx
+++ client/src/Components/FirstScreen/FirstScreen.tsx
@@ -6,9 +6,8 @@ export const FirstScreen = () => {
       <div className={styles.block}>
         <div className={styles.blockLeft}>
           <div>
-            <div className={styles.title}>
-              Курьерская служба доставки Люблино
-            </div>
+            <div className={styles.title}>Курьерская служба</div>
+            <div className={styles.title}>доставки Люблино</div>
           </div>
           <div className={styles.subtitle}>Отправляй и получай!</div>
           <div>
@@ -16,7 +15,7 @@ export const FirstScreen = () => {
           </div>
         </div>
         <div className={styles.blockRight}>
-          <div className={styles.track}>Трек номер</div>
+          <div className={styles.track}>Номер заказа</div>
           <div className={styles.trackInput}>
             <input type='text' placeholder='Введите номер заказа' />
           </div>
